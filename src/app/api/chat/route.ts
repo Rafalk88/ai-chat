9213c62c43b62ea36fb29fb1ec7bf3d8ae14db29
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-05-19 21:31:48 +0200

    feat: make chat api

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  const { prompt } = await req.json();
+
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: 'gtp-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }],
+    }),
+  });
+
+  const data = await response.json();
+  return NextResponse.json({ result: data.choices[0].messages.content });
+}
